fdc50c2bce430a1b34539972a99fec04750266b9
AgathaLuana-01 <agathaluana.ads@gmail.com>
2024-02-20 14:42:11 -0300

    implementação do titulo  e subtitulo

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
 import { Header } from '@components/Header';
+import { Highligth } from '@components/Higthligth';
 import { Container } from './styles';
 
 export function Groups() {
   return (
     <Container>
      <Header/>
+     <Highligth
+     title='Turmas'
+     subtitle='Jogue com sua turma'
+     />
     </Container>
   );
 }
