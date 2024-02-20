63398c206393e274c3db8c6ad4f1dcb3702f8062
AgathaLuana-01 <agathaluana.ads@gmail.com>
2024-02-20 14:04:28 -0300

    implementação status bar

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
-import { ActivityIndicator } from 'react-native'; //Componente de Load
+import { StatusBar } from "react-native";
 import { ThemeProvider } from "styled-components";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import theme from './src/theme';
 import { Groups } from "@screens/Groups";
+
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold }); //Criação de um vetor com variável boleana,que observa se as fontes foram carregadas, para que a fonte esteja disponivel antes de aparecer a aplicação para o usuário 
 
 
   return (
-    <ThemeProvider theme={theme}> 
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="trasparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
