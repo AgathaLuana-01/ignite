b38b127be2169c847bcc786e56943092f74f3f07
AgathaLuana-01 <agathaluana.ads@gmail.com>
2024-02-20 15:42:36 -0300

    implementação do card de tuma

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,15 +1,19 @@
 import { Header } from '@components/Header';
 import { Highligth } from '@components/Higthligth';
+import { GroupCard } from '@components/GroupCard';
+
 import { Container } from './styles';
 
 export function Groups() {
   return (
     <Container>
-     <Header/>
-     <Highligth
-     title='Turmas'
-     subtitle='Jogue com sua turma'
-     />
+      <Header />
+      <Highligth
+        title='Turmas'
+        subtitle='Jogue com sua turma'
+      />
+
+      <GroupCard title='Galera do Ignite' />
     </Container>
   );
 }
