5e97dbb130b7bad6d7c39e4ff155a02671c4b0ca
AgathaLuana-01 <agathaluana.ads@gmail.com>
2024-02-20 17:37:56 -0300

    criação da lista de grupos

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highligth } from '@components/Higthligth';
 import { GroupCard } from '@components/GroupCard';
@@ -5,6 +7,8 @@ import { GroupCard } from '@components/GroupCard';
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket', 'Amigos']);
+
   return (
     <Container>
       <Header />
@@ -13,7 +17,14 @@ export function Groups() {
         subtitle='Jogue com sua turma'
       />
 
-      <GroupCard title='Galera do Ignite' />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item}
+          />
+        )}
+      />
     </Container>
   );
 }
