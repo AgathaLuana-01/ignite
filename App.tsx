f4c0222f5b1ab001b99f0e2a6172ec910553e739
AgathaLuana-01 <agathaluana.ads@gmail.com>
2024-02-20 11:49:17 -0300

    Implementação da fonte Roboto na aplicação. Implementação do Activity Indicator caso a fonte não tenha carregado

--- App.tsx
+++ App.tsx
@@ -1,9 +1,17 @@
+import { ActivityIndicator } from 'react-native'; //Componente de Load
+import { ThemeProvider } from "styled-components";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import theme from './src/theme';
 import { Groups } from "@screens/Groups";
 
-
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold }); //Criação de um vetor com variável boleana,que observa se as fontes foram carregadas, para que a fonte esteja disponivel antes de aparecer a aplicação para o usuário 
+
+
   return (
-    <Groups/>
+    <ThemeProvider theme={theme}> 
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+    </ThemeProvider>
   );
 }
 
