a439f531c690ddce28be171c58f39c8e41153dd7
AgathaLuana-01 <agathaluana.ads@gmail.com>
2024-02-20 14:05:47 -0300

    criação do Header: Botão de voltar e logo

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { Container, Logo, BackIcon, BackButton } from "./styles";
+import logoImg from '@assets/logo.png'
+
+export function Header() {
+    return (
+        <Container>
+            <BackButton>
+                <BackIcon />
+            </BackButton>
+            <Logo source={logoImg} />
+        </Container>
+    )
+}
