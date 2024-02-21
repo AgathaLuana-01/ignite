068fdec1e3581ca2ca08b0cf28bbcea34af718f3
AgathaLuana-01 <agathaluana.ads@gmail.com>
2024-02-21 15:11:58 -0300

    implementação do input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Highligth } from '@components/Higthligth';
 import { Container, Content, Icon } from './styles';
 import { Header } from '@components/Header';
 import { Button } from '@components/Button';
+import { Input } from '@components/Input';
 
 export function NewGroup() {
   return (
@@ -10,10 +11,11 @@ export function NewGroup() {
       <Content>
         <Icon/>
         <Highligth
-        title='Nova Turma'
-        subtitle='Crie a turma para adcionar as pessoas'/>
+          title='Nova Turma'
+          subtitle='Crie a turma para adcionar as pessoas'/>
+        <Input />
         <Button
-        title='Criar'/>
+          title='Criar'/>
       </Content>
     </Container>
   );
