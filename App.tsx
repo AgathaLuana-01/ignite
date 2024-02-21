9af06b55affd897c150214ddc3207e3c65d7f9b7
AgathaLuana-01 <agathaluana.ads@gmail.com>
2024-02-21 16:26:18 -0300

    Mostrando a tela de Players

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 import theme from './src/theme';
 
 import { Loading } from '@components/Loading';
-import { NewGroup } from "@screens/NewGroup";
+import { Players } from "@screens/Players";
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold }); //Criação de um vetor com variável boleana,que observa se as fontes foram carregadas, para que a fonte esteja disponivel antes de aparecer a aplicação para o usuário 
 
@@ -16,7 +16,7 @@ export default function App() {
         backgroundColor="trasparent"
         translucent
       />
-      {fontsLoaded ? <NewGroup /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   );
 }
