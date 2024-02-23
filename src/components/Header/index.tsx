b54682e01c0e62188f81c7c59321386666a95f70
AgathaLuana-01 <agathaluana.ads@gmail.com>
2024-02-23 13:58:59 -0300

    Voltar para a tela incial usando navigate

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,25 @@
 import { Container, Logo, BackIcon, BackButton } from "./styles";
 import logoImg from '@assets/logo.png'
+import { useNavigation } from "@react-navigation/native";
 
 type Props = {
     showBackButton?: boolean;
 }
 
 export function Header({ showBackButton = false }: Props) {
+    const navigation = useNavigation();
+
+    function handleGoBack(){
+        navigation.navigate('groups');
+    }
+
     //showBackButton && // Se o showBackButton === true mostra <BackButton>
     return (
         <Container>
             {
                 showBackButton &&
-                <BackButton>
-                    <BackIcon />
+                <BackButton onPress={handleGoBack}>
+                    <BackIcon/>
                 </BackButton>
             }
             <Logo source={logoImg} />
