667b37dff888be5238c2b3c04e447ba14868e01e
AgathaLuana-01 <agathaluana.ads@gmail.com>
2024-02-23 17:16:55 -0300

    atualização dos dados async ao voltar para a tela inicial

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -6,13 +6,19 @@ import { Header } from '@components/Header';
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 import { useState } from 'react';
+import { groupCreate } from '@storage/group/groupCreat';
 
 export function NewGroup() {
   const [group, setGroup] = useState('');
   const navigation = useNavigation();
 
-  function handleNew() {
-    navigation.navigate('players', { group });
+  async function handleNew() {
+    try {
+      await groupCreate(group);
+      navigation.navigate('players', { group });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
