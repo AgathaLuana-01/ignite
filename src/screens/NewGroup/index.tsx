ae6397f7c481ae3b0143f15be1d99645ffa00400
AgathaLuana-01 <agathaluana.ads@gmail.com>
2024-02-23 14:20:53 -0300

    Passar parâmetros pela Rota

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -5,12 +5,14 @@ import { Container, Content, Icon } from './styles';
 import { Header } from '@components/Header';
 import { Button } from '@components/Button';
 import { Input } from '@components/Input';
+import { useState } from 'react';
 
 export function NewGroup() {
+  const [group, setGroup] = useState('');
   const navigation = useNavigation();
 
   function handleNew() {
-    navigation.navigate('players', { group: 'Rocket' });
+    navigation.navigate('players', { group });
   }
 
   return (
@@ -23,6 +25,7 @@ export function NewGroup() {
           subtitle='Crie a turma para adcionar as pessoas' />
         <Input
           placeholder='Nome da Turma'
+          onChangeText={setGroup}
         />
         <Button
           title='Criar'
