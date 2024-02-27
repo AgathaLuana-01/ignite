15d8c9a2598f1603ba0aae05e615457ffa3b59a8
AgathaLuana-01 <agathaluana.ads@gmail.com>
2024-02-26 21:09:43 -0300

    implementação useEffect

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Alert, FlatList } from "react-native";
 import { useRoute } from "@react-navigation/native";
 
@@ -42,7 +42,7 @@ export function Players() {
 
         try {
             await playerAddByGroup(newPlayer, group);
-
+            fetchPlayersByTeam();
         } catch (error) {
             if (error instanceof AppError) {
                 Alert.alert('Nova pessoa', error.message);
@@ -62,6 +62,11 @@ export function Players() {
             Alert.alert('Pessoas', 'Não foi possível carregar as pessoas do time selecionado.');
         }
     }
+
+    useEffect(() => {
+        console.log('useEffect executou')
+        fetchPlayersByTeam();
+    }, [team]);
 
     return (
         <Container>
@@ -102,10 +107,10 @@ export function Players() {
 
             <FlatList
                 data={players}
-                keyExtractor={item => item}
+                keyExtractor={item => item.name}
                 renderItem={({ item }) => (
                     <PlayerCard
-                        name={item}
+                        name={item.name}
                         onRemove={() => { }}
                     />
                 )}
